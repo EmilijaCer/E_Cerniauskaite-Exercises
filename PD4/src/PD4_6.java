a2254395519d5ac5c1283774567fa571b9b9d00a
Emilija.Cer <emilija.cerniauskaite@gmail.com>
2023-12-24 10:34:13 +0200

    commit

---
+++ PD4/src/PD4_6.java
@@ -0,0 +1,10 @@
+public class PD4_6 {
+    public static void main(String[] args) {
+        int intNum1 =33;
+        int intNum2= 87;
+
+
+        int rez = (intNum1 <= intNum2) ? (intNum1 * intNum2) : (intNum1 + intNum2);
+        System.out.println(rez);
+    }
+}
