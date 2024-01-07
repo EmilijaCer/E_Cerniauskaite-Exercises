eeaffe24b2b313d2e937c005ff3adcc6527a05b5
EmilijaCer <151562188+EmilijaCer@users.noreply.github.com>
2024-01-07 18:20:37 +0200

    Update PD5_1.java

--- PD5/src/PD5_1.java
+++ PD5/src/PD5_1.java
@@ -1,19 +1,8 @@
 public class PD5_1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++){ //Trikampis
+for (int i = 1; i <= 5; i++){ //Trikampis
             for (int j = 1; j <= i; j++){
-                if (j == 1 || j == i || i == 10){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-
-        for (int r = 1; r <= 15; r++){ //kvadratas
-            for (int c = 1; c <= 15; c++){
-                if (r == 1 || r == 15 || c == 1 || c == 15){
+                if (j == 1 || j == i || i == 5){
                     System.out.print(" *");
                 } else {
                     System.out.print("  ");
@@ -21,9 +10,21 @@ public class PD5_1 {
             }
             System.out.println();
         }
-        for (int r = 1; r <= 10; r++){ //staciakampis
-            for (int c = 1; c <= 15; c++){
-                if (r == 1 || r == 10 || c == 1 || c == 15){
+
+
+        for (int r = 1; r <= 5; r++){ //staciakampis
+            for (int c = 1; c <= 8; c++){
+                if (r == 1 || r == 5 || c == 1 || c == 8){
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int r = 1; r <= 5; r++){ //kvadratas
+            for (int c = 1; c <= 5; c++){
+                if (r == 1 || r == 5 || c == 1 || c == 5){
                     System.out.print(" *");
                 } else {
                     System.out.print("  ");
