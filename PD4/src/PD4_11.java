3251bceb6d4efecc9eddc86a8db29e5a91aa4205
EmilijaCer <151562188+EmilijaCer@users.noreply.github.com>
2024-01-07 18:05:09 +0200

    Update PD4_11.java

--- PD4/src/PD4_11.java
+++ PD4/src/PD4_11.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class PD4_11 {
     public static void main(String[] args) {
-            Scanner scanNumbers1 = new Scanner(System.in);
-            System.out.println("Koks automobilio greitis? ");
+           Scanner scanNumbers1 = new Scanner(System.in);
+            System.out.println("Koks automobilio greitis  km/h? ");
             double greitis = scanNumbers1.nextInt();
+
 
             double tunelioIlgis =  264;
             System.out.println(tunelioIlgis + " " + "m.");
 
-            double laikas = (tunelioIlgis * greitis)/1000;
-            System.out.println("Automobilis tunelį pravažiuos per" + " " +laikas+ " " + "s");
+            double laikas = (tunelioIlgis / ((greitis*1000)/3600));
+            System.out.println("Automobilis tunelį pravažiuos per" + " " +String.format("%.2f", laikas) + " " + "s");
         }
     }
